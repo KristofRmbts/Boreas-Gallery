1b31e44944d823e0e81067ad86b5c661ed6bd801
Kristof R <kristofrombauts@me.com>
2023-05-12 15:01:05 +0200

    Item details page

--- boreas-gallery-client/src/pages/Shop/ItemDetailsPage.jsx
+++ boreas-gallery-client/src/pages/Shop/ItemDetailsPage.jsx
@@ -28,65 +28,79 @@ function ItemDetailsPage (props) {
     <div className="body-container">
       {item && (
         <div className="item-container">
-        <div className="item-container-left">
-        <br />
-        <img src={item.imageUrl} alt="Item preview" className="item-image" />
-        <br /><br /><br /><br />
-        <p className="text-s link-black">DELIVERY AND RETURNS</p>
-        <p className="text-s justify-text">Orders ship promptly after printing. All domestic orders are insured and trackable. International orders are insured and optionally trackable. No returns as all prints are custom made. An email containing tracking information will be sent once the order ships.</p>
-        </div>
-        <div className="item-container-right">
-          <h2 className="item-title">{item.title}</h2>
+          <div className="item-container-left">
           <br />
-          <p className="shop-item-info link-black">DETAILS</p>
-          <p className="shop-item-info">{item.price}.00€</p>
-          <br />
-          <p className="shop-item-info">{item.description}</p>
-          <br />
-          <p className="shop-item-info">{item.border}</p>
-          <div className="form-container">
-            <label className="form-label">Size</label><br />
-            <input
-            type="text"
-            name="size"
-            placeholder={item.size}
-            // onChange={(e) => setSize(e.target.value)}
-            className="form-input"
-            />
-            <br /><br />
-
+          <img src={item.imageUrl} alt="Item preview" className="item-image" />
+          <br /><br /><br /><br />
+          <p className="text-s link-black">DELIVERY AND RETURNS</p>
+          <p className="text-s justify-text">Orders ship promptly after printing. All domestic orders are insured and trackable. International orders are insured and optionally trackable. No returns as all prints are custom made. An email containing tracking information will be sent once the order ships.</p>
+          </div>
+          <div className="item-container-right">
+            <h2 className="item-title">{item.title}</h2>
+            <br />
+            <p className="shop-item-info link-black">DETAILS</p>
+            <p className="shop-item-info">{item.price}.00€</p>
+            <br />
+            <p className="shop-item-info">{item.description}</p>
+            <br />
+            <p className="shop-item-info">Limited edition of 20</p>
+            <p className="shop-item-info">Numbered and signed</p>
+            <br />
+            <p className="shop-item-info">Printing info 1</p>
+            <p className="shop-item-info">Printing info 2</p>
+            <p className="shop-item-info">Printing info 3</p>
+            <p className="shop-item-info">Printing info 4</p>
+            <p className="shop-item-info">Printing info 5</p>
+            <br />
             <div className="form-container">
-            <label className="form-label">Material</label><br />
-            <input
-            type="text"
-            name="material"
-            placeholder={item.material}
-            // onChange={(e) => setMaterial(e.target.value)}
-            className="form-input"
-            />
-            <br /><br />
+              <label className="form-label">Size</label><br />
+              <select
+              name="border"
+              className="form-select"
+              >
+              {item.size.map((size) => (
+                <>
+                  <option value={size}>{size}</option>
+                </>
+              ))}
+              </select>
+              <br /><br />
 
-            <label className="form-label">Border</label><br />
-            <input
-            type="text"
-            name="material"
-            placeholder={item.border}
-            // onChange={(e) => setMaterial(e.target.value)}
-            className="form-input"
-            />
-            <br /><br />
+              <label className="form-label">Material</label><br />
+              <select
+              name="material"
+              className="form-select"
+              >
+              {item.material.map((material) => (
+                <>
+                  <option value={material}>{material}</option>
+                </>
+              ))}
+              </select>
+              <br /><br />
+
+              <label className="form-label">Border</label><br />
+              <select
+              name="border"
+              className="form-select"
+              >
+              {item.border.map((border) => (
+                <>
+                  <option value={border}>{border}</option>
+                </>
+              ))}
+              </select>
+              <br /><br />
             </div>
+            <br />
+            <button type="submit" className="form-button">Send request</button>
           </div>
         </div>
-      </div>
       )}
-      <Link to="/shop">
-        <button>Back</button>
-      </Link>
-
-      <Link to={`/shop/${itemId}/edit`}>
+      <br /><br />
+      {/* <Link to={`/shop/${itemId}/edit`}>
         <button>Edit</button>
-      </Link>    
+      </Link>     */}
     </div>
   );
 }
