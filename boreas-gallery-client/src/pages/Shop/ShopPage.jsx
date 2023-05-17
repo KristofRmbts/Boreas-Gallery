2cd55764099dd0b4c2c559dcb164d6dbfad103c4
Kristof R <kristofrombauts@me.com>
2023-05-17 15:34:12 +0200

    deploy fix

--- boreas-gallery-client/src/pages/Shop/ShopPage.jsx
+++ boreas-gallery-client/src/pages/Shop/ShopPage.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-// import { Link } from "react-router-dom";
-// import AddItem from "../../components/AddItem";
+
 import ItemCard from "../../components/ItemCard";
 
 const API_URL = import.meta.env.FRONTEND_URL || "http://localhost:5005";
